b98c890d37c23cc8287947006e06e72035eda967
david <david@apollosoftware.com.au>
2012-04-10 15:20:19 +1000

    handle no user

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     info = request.env["omniauth.auth"]["info"]
     @user = User.find_for_google_auth(info)
-    sign_in_and_redirect @user unless @user.nil?
+    if @user.nil?
+      flash[:notice] = "No user found for #{info.email}"
+      redirect_to root_path
+    else
+      sign_in_and_redirect @user
+    end
+
    end
 
   def ldap
